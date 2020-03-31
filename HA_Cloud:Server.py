8f7b6c816177bf087486ec0c11a99114f041c41f
Luca Filippini <lucafilippini1@gmail.com>
2020-03-31 13:41:47 +0200

    Create HA_Cloud:Server.py

---
+++ HA_Cloud:Server.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+from os import curdir, sep
+from uuid import uuid4
+
+PORT_NUMBER = 8080
+
+# Handling richieste HTTP
+class HA_Cloud_Handler(BaseHTTPRequestHandler):
+	
+	# Handler richieste GET
+	def do_GET(self):
+  
+		if self.path=="/":
+			self.path="/index_.html"
+            
+
+    session_id = uuid4()
+    #UUID('6f4a1f4d-1315-4e3e-a737-14f005f86b8c')
+
+		try:
+			#Check the file extension required and
+			#set the right mime type
+
+			sendReply = False
+			if self.path.endswith(".html"):
+				mimetype='text/html'
+				sendReply = True
+			if self.path.endswith(".jpg"):
+				mimetype='image/jpg'
+				sendReply = True
+			if self.path.endswith(".gif"):
+				mimetype='image/gif'
+				sendReply = True
+			if self.path.endswith(".js"):
+				mimetype='application/javascript'
+				sendReply = True
+			if self.path.endswith(".css"):
+				mimetype='text/css'
+				sendReply = True
+
+			if sendReply == True:
+				#Open the static file requested and send it
+				f = open(curdir + sep + self.path) 
+				self.send_response(200)
+				self.send_header('Content-type',mimetype)
+				self.end_headers()
+				self.wfile.write(f.read())
+				f.close()
+			return
+
+
+		except IOError:
+			self.send_error(404,'File Not Found: %s' % self.path)
+
+try:
+	#Create a web server and define the handler to manage the
+	#incoming request
+	server = HTTPServer(('', PORT_NUMBER), myHandler)
+	print 'Started httpserver on port ' , PORT_NUMBER
+	
+	#Wait forever for incoming htto requests
+	server.serve_forever()
+
+except KeyboardInterrupt:
+	print '^C received, shutting down the web server'
+	server.socket.close()
